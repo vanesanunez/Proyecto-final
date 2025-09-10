60babaf4775723e86b31861ae82877024f5745ee
vanesanunez <142461430+vanesanunez@users.noreply.github.com>
2025-09-10 04:23:59 -0300

    Patrón observer autenticación

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,23 +1,62 @@
 <script>
 import AppH1 from '../components/AppH1.vue';
+import { login } from '../services/auth';
 
 export default {
     name: 'Login',
     components: { AppH1, },
+    data() {
+        return {
+            user: {
+                email: '',
+                password: '',
+            },
+            loading: false,
+        }
+    },
+    methods: {
+        async handleSubmit() {
+            try {
+                this.loading = true;
+                const user = await login(this.user.email, this.user.password);
+                this.loading = false;
+                // //emitimos el evento login con los datos del usuario
+                // this.$emit('login', {id: this.user.id, email: this.user.email});
+            } catch (error) {
+                //manejar el error
+            }
+        }
+    }
 }
 </script>
 
 <template>
     <AppH1>Ingresar a mi cuenta</AppH1>
-    <form action="#">
-        <div class="mb-3">
+
+    <form action="#"
+          @submit.prevent="handleSubmit"
+    >
+        <div class="mb-4">
             <label for="email" class="mb-2">Email</label>
-            <input type="email" id="email" class="form-control">
+            <input 
+                type="email" 
+                id="email" 
+                class="w-full p-2 border border-blue-300 rounded"
+                v-model="user.email"
+                >
         </div>
-        <div class="mb-3">
-            <label for="password" class="mb-2">Contraseña</label>
-            <input type="password" id="password" class="form-control">
+        <div class="mb-4">
+            <label for="password" class="block mb-2">Contraseña</label>
+            <input 
+                type="password" 
+                id="password" 
+                class="w-full p-2 border border-blue-300 rounded"
+                v-model="user.password"
+                >
         </div>
-        <button type="submit" class="">Ingresar</button>
+        <button type="submit" class="w-full transition px-4 py-2 rounded cursor-pointer bg-blue-600 hover:bg-blue-500 focus:bg-blue-500 active:bg-blue-700 text-white"
+        >Iniciar sesión
+        </button>
     </form>
+
 </template>
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,13 @@
 import supabase from "./supabase";
 
+//defino una variable que contenga los datos del usuario ("subject")
+let user = {
+    id: null,
+    email: null,
+}
+//Array para guardar la lista de observers que deben ser notificados de los cambios en "user"
+let observers = [];
+
 export async function register(email, password) {
     //método signUp() de auth de supabase
     const { data, error } = await supabase.auth.signUp({
@@ -13,9 +21,34 @@ export async function register(email, password) {
     console.log("Usuario registrado: ", data);
     
 }
+
 export async function login(email, password) {
-    
+     //método signInWithPassword() de auth de supabase
+     const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+    if(error) {
+        console.error('[auth.js login] Error al iniciar sesión: ', error);
+        throw error;
+    }
+    console.log("Usuario autenticado: ", data);
+    return data.user;
 }
+
 export async function logout() {
     
+}
+
+//Métodos para el observer
+
+/**
+ * @param {({id: string|null, email:string|null}) => void} callback
+ */
+export function subscribeToUserState(callback){
+    //Agregamos el callback al stack de observers
+    observers.push(callback);
+
+    //ejecutamos el callback para pasarle los datos actuales
+    notify(callback);
 }
