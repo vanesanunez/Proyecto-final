b0dd267c5f9222908616b0fb01ec90930df24dae
vanesanunez <142461430+vanesanunez@users.noreply.github.com>
2025-09-10 04:40:20 -0300

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -18,8 +18,13 @@ export async function register(email, password) {
         console.error('[auth.js register] Error al crear una cuenta: ', error);
         throw error;
     }
-    console.log("Usuario registrado: ", data);
-    
+    //guardar los datos del usuario autenticado notificar a los observers del cambio
+    user = {
+        ...user,
+        id: data.user.id,
+        email: data.user.email,
+    }
+    notifyAll();
 }
 
 export async function login(email, password) {
@@ -32,13 +37,28 @@ export async function login(email, password) {
         console.error('[auth.js login] Error al iniciar sesión: ', error);
         throw error;
     }
-    console.log("Usuario autenticado: ", data);
+    //guardar los datos del usuario autenticado notificar a los observers del cambio
+    user = {
+        ...user,
+        id: data.user.id,
+        email: data.user.email,
+    }
+    notifyAll();
     return data.user;
 }
 
 export async function logout() {
-    
+    supabase.auth.signOut();
+
+    user = {
+        ...user,
+        id: null,
+        email: null,
+    }
+    notifyAll();
+    return data.user;
 }
+
 
 //Métodos para el observer
 
@@ -51,4 +71,19 @@ export function subscribeToUserState(callback){
 
     //ejecutamos el callback para pasarle los datos actuales
     notify(callback);
+}
+/**
+ * invoca un observer y le pasa los datos del usuario
+ * @param {({id: string|null, email:string|null}) => void} callback
+ */
+export function notify(callback){
+   callback({...user});
+}
+
+/**
+ * notifica a todos los observers cada vez que ocurra un cambio (en la variable "user")
+ */
+function notifyAll() {
+    observers.forEach(callback => notify(callback));
+    //observers.forEach(notify);
 }
