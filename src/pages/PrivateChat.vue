2bdb7833e6ee3a7f6ba5bd160e7195c0f145b155
vanesanunez <142461430+vanesanunez@users.noreply.github.com>
2025-09-18 05:13:04 -0300

    chat privado

--- src/pages/PrivateChat.vue
+++ src/pages/PrivateChat.vue
@@ -1,12 +1,101 @@
 <script>
 import AppH1 from '../components/AppH1.vue';
+import MainButton from '../components/MainButton.vue';
+import MainLoader from '../components/MainLoader.vue';
+import { subscribeToUserState } from '../services/auth';
+import { getUserProfileById } from '../services/user-profiles';
 
 export default {
     name: 'PrivateChat',
-    components: {AppH1},
+    components: { AppH1, MainLoader, MainButton },
+    data() {
+        return {
+            userAuth: {
+                id: null,
+                email: null,
+                name: null,
+                lastname: null,
+            },
+
+            userChat: {
+                id: null,
+                email: null,
+                name: null,
+                lastname: null,
+            },
+            loadingUser: false,
+
+            messages: [],
+            loadingMessages: false,
+
+            NewMessage: {
+                body: '',
+            }
+        }
+    },
+    methods: {
+        async sendMessage() {
+
+        }
+    },
+    async mounted() {
+        try {
+            subscribeToUserState(newDataUser => this.userAuth = newDataUser);
+
+            this.loadingUser = true;
+            this.loadingMessages = true;
+
+            this.userChat = await getUserProfileById(this.$route.params.id);
+            this.loadingUser = false;
+        } catch (error) {
+            //manejar error
+        }
+    }
 }
 </script>
 
 <template>
-    <AppH1>Chat privado con @user</AppH1>
+    <template v-if="!loadingUser">
+        <AppH1>Chat privado con {{ userChat.email }}</AppH1>
+        <section 
+            ref="chatContainer" 
+            class="overflow-y-auto h-100 p-4 mb-4 border border-blue-400 rounded">
+
+            <h2 class="sr-only">Lista de mensajes</h2>
+
+            <ul v-if="!loadingMessages" 
+                class="flex flex-col gap-4">
+                <li 
+                    v-for="message in messages" 
+                    :key="message.id" 
+                    class="flex flex-col gap-0.5">
+                    
+                    <div>{{ message.body }}</div>
+                    <div class="text-sm text-gray-600">{{ message.created_at }}</div>
+                </li>
+            </ul>
+            <div v-else class="flex justify-center items-center h-full">
+                <MainLoader />
+            </div>
+        </section>
+        <section>
+            <h2 class="sr-only">Enviar un mensaje</h2>
+
+            <form 
+                class="flex items-stretch gap-4" 
+                action="#" @submit.prevent="() => sendMessage()"
+            >
+                <div class="w-full">
+                    <label for="body" class="sr-only">Mensaje</label>
+                    <textarea 
+                        id="body" 
+                        class="w-full p-2 border border-r-blue-400 rounded"
+                        v-model="NewMessage.body"
+                        ></textarea>
+                </div>
+                <MainButton>Enviar</MainButton>
+            </form>
+        </section>
+    </template>
+    <MainLoader v-else />
 </template>
