024a22a7c2d6ce82b01fc41aba958d40a71721b6
vanesanunez <142461430+vanesanunez@users.noreply.github.com>
2025-10-23 01:40:12 -0300

    Update Routes.vue

--- src/pages/Routes.vue
+++ src/pages/Routes.vue
@@ -3,11 +3,15 @@ import AppH1 from '../components/AppH1.vue';
 
 export default {
     name: 'Routes',
-    components: {AppH1}
+    components: { AppH1 }
 }
 
 </script>
 
 <template>
-    <AppH1>Recorrido seguro</AppH1>
+    <AppH1>¡Buscá el recorrido más seguro!</AppH1>
+    <p>Gracias a los aportes de la comunidad, te indicamos las mejores rutas o caminos sugeridos hacia tu destino (más
+        transitados, mejor iluminados, etc.)
+        También podés compartir el trayecto en tiempo real con tus contactos de confianza.
+    </p>
 </template>
