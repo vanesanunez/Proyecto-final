29f6b55058af8147c9c484da796e44b217090e85
tatilai <ramirezlaila.t23@gmail.com>
2025-10-27 18:47:49 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,27 +7,3 @@ export default {
     
 
 
-/*
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
-
-// ✅ Configuración de Vite para Vue + Tailwind + Proxy Nominatim
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  server: {
-    proxy: {
-      '/nominatim': {
-        target: 'https://nominatim.openstreetmap.org',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/nominatim/, ''),
-        headers: {
-          'User-Agent': 'ViaSegura/1.0 (proyecto escolar - Escuela Da Vinci)',
-          'Accept-Language': 'es'
-        }
-      }
-    }
-  }
-})
-
-*/
